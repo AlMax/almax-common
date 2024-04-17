c02afc53391c950b1b55930cc03769b2e0881726
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-04-18 00:26:57 +0200

    dev

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='almax_common',
     version='0.4.0',
     description='A common library with some of my implementations',
-    long_description=long_description,
+    long_description='test',
     long_description_content_type='text/markdown',
     author='AlMax98',
     author_email='alihaider.maqsood@gmail.com',
