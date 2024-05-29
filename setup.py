fc06433d7d93811282963a06b1b482167c9c0f91
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-05-29 23:11:52 +0200

    version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='almax_common',
-    version='0.9.0',
+    version='0.9.1',
     description='A common library with some of my implementations',
     long_description='test',
     long_description_content_type='text/markdown',
