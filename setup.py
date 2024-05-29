90562a9584ee99f9367d925ab537de2452502bbe
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-05-30 00:21:38 +0200

    fixes

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open("requirements.txt") as f:
 
 setup(
     name='almax_common',
-    version='0.9.2',
+    version='0.9.3',
     description='A common library with some of my implementations',
     long_description='test',
     long_description_content_type='text/markdown',
     author='AlMax98',
     author_email='alihaider.maqsood@gmail.com',
-    packages=find_packages(),
-    install_requires=[],
-    py_modules=['Common']
+    packages=find_packages(where='.'),  # Ensure it finds the Common package
+    package_dir={'': '.'},  # Specify the root directory as the package directory
+    install_requires=[],  # Add any dependencies here
 );
