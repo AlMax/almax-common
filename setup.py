db2ef622e92696a460d56f403d667a06c0b0636e
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-06-01 17:29:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ with open("README.md", "r") as fh:
 with open("requirements.txt") as f:
     required = f.read().splitlines();
 
+time = Time.now.strftime('%Y%m%d%H%M%S');
 setup(
     name='almax_common',
-    version=f"0.11.{Time.now.strftime('%Y%m%d%H%M%S')}",
+    version=f"0.11.{time}",
     description='A common library with some of my implementations',
     long_description=readMe,
     long_description_content_type='text/markdown',
