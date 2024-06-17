0bd4a08c471b635acfa1e72680d24605498179ec
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-06-17 20:38:14 +0200

    deps installation

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages;
+import argparse;
 
 with open("README.md", "r") as fh:
     readMe = fh.read();
@@ -8,6 +9,38 @@ with open("requirements.txt") as f:
 
 with open("version.txt", "r") as fh:
     versionFile = fh.read().strip();
+
+optional_dependencies = {
+    "All": [],
+    "Graphics": [
+        "tkcalendar==1.6.1"
+    ],
+    "PdfManager": [
+        "reportlab==4.2.0",
+        "pypdf==4.2.0"
+    ],
+    "XmlManager": [
+        "lxml==5.2.2"
+    ],
+    "AdvancedUtils": [
+        "codicefiscale==0.9"
+    ]
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--library", action="append", help="Libraries Needed (comma-separated)")
+args, unknown = parser.parse_known_args()
+libraries = args.library
+
+installRequiresCustom = required;
+if libraries:
+    for library in libraries:
+        if library in optional_dependencies:
+            if library == "All":
+                for lib,dep in optional_dependencies:
+                    installRequiresCustom.extend(dep);
+            else:
+                installRequiresCustom.extend(optional_dependencies[library]);
 
 setup(
     name='almax_common',
@@ -19,5 +52,5 @@ setup(
     author_email='alihaider.maqsood@gmail.com',
     packages=find_packages(),
     package_dir={'': '.'},
-    install_requires=required,
+    install_requires=installRequiresCustom,
 );
